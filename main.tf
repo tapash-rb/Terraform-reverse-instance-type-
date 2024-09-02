bfb7dbaa3b9d939ba53fddb3cf03e6c1a1a94e4b
Ubuntu <ubuntu@ip-172-31-39-72.ap-south-1.compute.internal>
2024-09-02 18:00:48 +0000

    project

---
+++ main.tf
@@ -0,0 +1,23 @@
+/*resource "aws_instance" "my_instance" {
+  ami           = "ami-0c2af51e265bd5e0e" # Replace with a valid Ubuntu AMI ID for your region
+  instance_type = "t2.micro"
+  tags = {
+    Name = "MyInstance" 
+  }
+} */
+
+
+provider "aws" {
+  region = "ap-south-1"  # Change to your preferred region
+}
+
+resource "aws_instance" "my_instance-1" {
+  ami           = "ami-0a0e5d9c7acc336f1"  # Same AMI ID
+  instance_type = "t2.medium"  # Updated instance type
+
+  tags = {
+    Name = "MyInstance-1"
+  }
+}
+
+
